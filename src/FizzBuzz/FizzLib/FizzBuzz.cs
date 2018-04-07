a7f5c0b928f4eb20603c978326dc9f91bee4e1b6
DJ Soults <dj@illuminateddev.com>
2018-04-07 18:44:44 -0500

    Initial project structure

---
+++ src/FizzBuzz/FizzLib/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzLib
+{
+    public class FizzBuzz
+    {
+        public IDictionary<int, string> GetFizzBuzz(int min = 1, int max = 100)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
