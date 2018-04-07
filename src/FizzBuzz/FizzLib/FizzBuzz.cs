9d0ef77fafae32e3034a7cda91950dda0ec4394b
DJ Soults <dj@illuminateddev.com>
2018-04-07 18:48:12 -0500

    Library implementation

--- src/FizzBuzz/FizzLib/FizzBuzz.cs
+++ src/FizzBuzz/FizzLib/FizzBuzz.cs
@@ -7,7 +7,28 @@ namespace FizzLib
     {
         public IDictionary<int, string> GetFizzBuzz(int min = 1, int max = 100)
         {
-           throw new NotImplementedException();
+            var dictionary = new Dictionary<int, string>();
+
+            for (var i = min; i < max; i++)
+            {
+                var value = "";
+                value = value + GetFizz(i);
+                value = value + GetBuzz(i);
+
+                dictionary.Add(i, value);
+            }
+
+            return dictionary;
+        }
+
+        private static string GetFizz(int i)
+        {
+            return i % 3 == 0 ? "fizz" : i.ToString();
+        }
+
+        private static string GetBuzz(int i)
+        {
+            return i % 5 == 0 ? "buzz" : i.ToString();
         }
     }
 }
