72184735158362fb512299243a90b9a21ab55c29
DJ Soults <dj@illuminateddev.com>
2018-04-07 19:20:46 -0500

    Added iterative approach to avoid memory issues

--- src/FizzBuzz/FizzClient/Program.cs
+++ src/FizzBuzz/FizzClient/Program.cs
@@ -1,25 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FizzLib;
 
 namespace FizzClient
 {
     class Program
     {
+        private const int ITERATOR_MAX = 5000;
+
         static void Main(string[] args)
         {
-            var fizzBuzz = new FizzBuzz();
+            RunForRange(1, 100);
 
-            Console.WriteLine("Running from 1 to 100");
-            Console.WriteLine();
-            var defaultRun = fizzBuzz.GetFizzBuzz();
-            PrintValues(defaultRun);
+            RunForRange(1, Int32.MaxValue);
+
+            RunForRange(Int32.MinValue, Int32.MaxValue);
 
             Console.WriteLine(@"Press Enter To Close...");
             Console.ReadLine();
+        }
+
+        private static void RunForRange(int min, int max)
+        {
+            Console.WriteLine($"Running from {min} to {max}");
+            Console.WriteLine();
+
+            var i = min;
+            while (i <= max)
+            {
+                var iterMin = i;
+                i += ITERATOR_MAX;
+                var iterMax = Math.Min(i, max);
+
+                var run = new FizzBuzz().GetFizzBuzz(iterMin, iterMax);
+                PrintValues(run);
+            }
+
+            Console.WriteLine();
         }
 
         private static void PrintValues(IDictionary<int, string> run)
