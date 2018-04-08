3e9ff04997fad1b92586a7ca86574d764b465152
DJ Soults <dj@illuminateddev.com>
2018-04-07 19:00:21 -0500

    Initial console implementation and initial build commit

--- src/FizzBuzz/FizzClient/Program.cs
+++ src/FizzBuzz/FizzClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzLib;
 
 namespace FizzClient
 {
@@ -10,7 +11,23 @@ namespace FizzClient
     {
         static void Main(string[] args)
         {
-            
+            var fizzBuzz = new FizzBuzz();
+
+            Console.WriteLine("Running from 1 to 100");
+            Console.WriteLine();
+            var defaultRun = fizzBuzz.GetFizzBuzz();
+            PrintValues(defaultRun);
+
+            Console.WriteLine(@"Press Enter To Close...");
+            Console.ReadLine();
+        }
+
+        private static void PrintValues(IDictionary<int, string> run)
+        {
+            foreach (var row in run)
+            {
+                Console.WriteLine($"{row.Key}: {row.Value}");
+            }
         }
     }
 }
